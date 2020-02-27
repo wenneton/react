0e2a446d7cd8567b69f3849b57b1a58497f08b30
Marcos Wenneton <wenneton@outlook.com>
2020-02-27 17:07:54 -0400

    Assignment 2 - First Submission

--- confusao/src/components/FooterComponent.js
+++ confusao/src/components/FooterComponent.js
@@ -18,12 +18,12 @@ function Footer(props) {
                 <div className="col-7 col-sm-5">
                     <h5>Our Address</h5>
                     <address>
-		              121, Clear Water Bay Road<br />
-		              Clear Water Bay, Kowloon<br />
-		              HONG KONG<br />
-		              <i className="fa fa-phone fa-lg"></i>: +55 1234 5678<br />
-		              <i className="fa fa-fax fa-lg"></i>: +55 8765 4321<br />
-		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:caofusao@food.net">
+                      121, Clear Water Bay Road<br />
+                      Clear Water Bay, Kowloon<br />
+                      HONG KONG<br />
+                      <i className="fa fa-phone fa-lg"></i>: +55 1234 5678<br />
+                      <i className="fa fa-fax fa-lg"></i>: +55 8765 4321<br />
+                      <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:caofusao@food.net">
                          caofusao@food.net</a>
                     </address>
                 </div>
--- confusao/src/components/MainComponent.js
+++ confusao/src/components/MainComponent.js
@@ -12,6 +12,7 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import Dishdetail from './DishdetailComponent';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -52,7 +53,8 @@ class Main extends Component {
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
           <Route path='/menu/:dishId' component={DishWithId} />
-          <Route exact path='/contactus' component={Contact} />} />
+          <Route exact path='/contactus' component={Contact} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders}/>} />
           <Redirect to='/home' />
         </Switch>
         <Footer />
